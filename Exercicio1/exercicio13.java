6e2dd56b5bc522a391361da1fb7da04976a6b74e
maykongarciadias-cmyk <maykongarciadias@gmail.com>
2026-09-02 19:43:28 -0300

    Add files via upload

---
+++ Exercicio1/exercicio13.java
@@ -0,0 +1,8 @@
+public class exercicio13 {
+    public static void main(String[] args) { 
+
+int numero = 5; for (int i = 1; i <= 10; i++) {
+ System.out.printf("%d x %d = %d%n", numero, i, numero * i); 
+} 
+} 
+} 
