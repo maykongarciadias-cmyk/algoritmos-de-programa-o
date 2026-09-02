4674f22decb147e765c85f3d29ede2c6a0ea2b4c
maykongarciadias-cmyk <maykongarciadias@gmail.com>
2026-09-02 18:45:51 -0300

    Add files via upload

---
+++ Exercicio1/exercicio8.java
@@ -0,0 +1,13 @@
+public class exercicio8 {
+    public static void main(String[] args) {
+        System.err.println("Nota 1: 8.0");
+        System.err.println("Nota 2: 7.5");
+        System.err.println("Nota 3: 9.0");
+
+        double media = (8.0+7.5+9.0)/3;
+        System.out.println("A média é: "+media);
+
+    
+    }
+    
+}
