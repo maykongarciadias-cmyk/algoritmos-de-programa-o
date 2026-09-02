b9b885bf31b78a4847fb43397280f090e54c72ee
maykongarciadias-cmyk <maykongarciadias@gmail.com>
2026-09-02 09:52:33 -0300

    Add files via upload

---
+++ Exercicio1/exercicio1.java
@@ -0,0 +1,9 @@
+public class exercicio1 {
+    public static void main(String[] args){
+
+        System.out.println("O seu nome completo completo é: MAykon Garcia Dias Do Nascimento ");
+        System.out.println("RA: 06346916");
+        System.out.println("Curso: Analise e desenvolvimento de sistemas");
+    }
+    
+}
