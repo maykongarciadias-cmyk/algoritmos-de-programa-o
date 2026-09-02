91def56e12c3cda9e9437b032f888044924d0d44
maykongarciadias-cmyk <maykongarciadias@gmail.com>
2026-09-02 18:39:45 -0300

    Add files via upload

---
+++ Exercicio1/exercicio7.java
@@ -0,0 +1,21 @@
+public class exercicio7 {
+    public static void main(String[] args){
+
+        boolean x1 = 120 - 30 == Math.pow(3.0,30.0);
+        System.out.println("((120 - 30) = (3 ˆ 30)) = "+ x1);
+
+        boolean x2 = (20%4.0/2==1||9!=9);
+        System.out.println("(não ((20 módulo 4/2)= 1) ou (9 != 9)) = "+!x2);
+
+        boolean x3 = 5%2>3;
+        System.out.println("((5 módulo 2 ) > 3) = "+x3);
+
+
+        char a = 'a';
+        char A = 'A';
+
+        boolean x4 = (a==A);
+        System.err.println("(a=A) = " +x4);
+    }
+    
+}
