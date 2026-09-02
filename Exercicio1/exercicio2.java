9956dbe52646c750ffca00c8581af9be566f2280
maykongarciadias-cmyk <maykongarciadias@gmail.com>
2026-09-02 11:04:08 -0300

    Add files via upload

---
+++ Exercicio1/exercicio2.java
@@ -0,0 +1,12 @@
+public class exercicio2 {
+    public static void main(String[] args){
+
+        System.out.println("A sua idade é: ");
+        System.out.println("23 anos");
+        System.out.println("A sua altura é: ");
+        System.out.println("1,74 metros");
+        System.out.println("O seu status na aula é: ");
+        System.out.println("Presente");
+    }
+    
+}
