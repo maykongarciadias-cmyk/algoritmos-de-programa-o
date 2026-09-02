676a7e15b82f76b8b9b8cdff4ef34f8aba505e8f
maykongarciadias-cmyk <maykongarciadias@gmail.com>
2026-09-02 18:55:28 -0300

    Add files via upload

---
+++ Exercicio1/exercicio10.java
@@ -0,0 +1,9 @@
+public class exercicio10 {
+    public static void main(String[] args) {
+
+        double x = 5;
+        double y = Math.PI*Math.pow(x,2);
+        System.out.println("A área do circula é: "+y);
+    }
+    
+}
